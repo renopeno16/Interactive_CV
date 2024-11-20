ae047a1af5e0cf241324aa54b040faac10f9ada6
renopeno16 <renzopel@hotmail.com>
2024-11-20 10:26:17 -0500

    Initial commit

---
+++ Interactive CV real/scripts.js
@@ -0,0 +1,8 @@
+﻿function toggleSection(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
